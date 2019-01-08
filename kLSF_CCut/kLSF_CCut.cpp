0bc7590e66d17bfae82a56a2ea423d78f8e85ceb
HueBr <pedro.jorge@aluno.uece.br>
2019-01-08 16:17:30 -0300

    prototipo funcional

--- kLSF_CCut/kLSF_CCut.cpp
+++ kLSF_CCut/kLSF_CCut.cpp
@@ -4,6 +4,7 @@
 #include <boost/graph/graph_traits.hpp> // for creation of descriptors vertex and edges.
 #include <boost/graph/adjacency_list.hpp> //for usage of adjacency list
 #include <boost/graph/filtered_graph.hpp>
+#include <boost/graph/connected_components.hpp>
 #include <ilcplex/ilocplex.h>
 #include <unordered_set>
 #include <boost/dynamic_bitset.hpp>
@@ -12,17 +13,118 @@ ILOSTLBEGIN //initialization make vs work properly
 using namespace boost;
 
 //basic definitions
-typedef adjacency_list<vecS, vecS, undirectedS, no_property, property<edge_color_t, int>> graph_t;
+typedef typename adjacency_list<vecS, vecS, undirectedS, no_property, property<edge_color_t, int>> graph_t;
 typedef dynamic_bitset<> db;
+
+template <typename EdgeColorMap, typename ValidColorsMap>
+struct valid_edge_color {
+	valid_edge_color() { }
+	valid_edge_color(EdgeColorMap color, ValidColorsMap v_colors) : m_color(color), v_map(v_colors) { }
+	template <typename Edge>
+	bool operator()(const Edge& e) const {
+		return v_map.test(get(m_color, e));
+	}
+	EdgeColorMap m_color;
+	ValidColorsMap v_map;
+};
+
+
+
+template<class Graph, class Mask>
+void print_filtered_graph(Graph &g, Mask valid) { //pay atention to the position of the bits and the colors positions in array
+	typedef typename property_map<Graph, edge_color_t>::type EdgeColorMap;
+	typedef typename boost::dynamic_bitset<> db;
+	typedef filtered_graph<Graph, valid_edge_color<EdgeColorMap, db> > fg;
+
+	valid_edge_color<EdgeColorMap, Mask> filter(get(edge_color, g), valid);
+	fg tg(g, filter);
+	print_edges(edges(tg).first, edges(tg).second, tg);
+}
+template<class Graph, class Mask>
+int get_components(Graph &g, Mask &m, vector<int> &components) {
+	typedef typename property_map<Graph, edge_color_t>::type EdgeColorMap;
+	typedef typename boost::dynamic_bitset<> db;
+	typedef filtered_graph<Graph, valid_edge_color<EdgeColorMap, db> > fg;
+
+	valid_edge_color<EdgeColorMap, Mask> filter(get(edge_color, g), m);
+	fg tg(g, filter);
+	int num = connected_components(tg, &components[0]);
+	return num;
+}
+
+template<class Graph>
+property_map<graph_t, edge_color_t>::type get_colors(Graph &g) {
+	typedef typename property_map<Graph, edge_color_t>::type ColorMap;
+	ColorMap colors = get(edge_color,g);
+	//make color type generic
+	return colors;
+}
+
+
 //Callbacks new to me new to you let god save my soul
 
-ILOUSERCUTCALLBACK2(MyCuts, IloBoolVarArray,z,graph_t,g) {
+ILOUSERCUTCALLBACK2(MyCuts, IloBoolVarArray, z, graph_t, g) {
 	int size = z.getSize();
 	db temp(size);
-	for (int i = 0; i < size; ++i) {
-		if (getValue(z[i])) temp.set(i);
+	std::vector<int> components(num_vertices(g));
+	auto colors = get_colors(g);
+	graph_traits<graph_t>::edge_iterator it, end;
+	for (int i = 0; i < size; ++i) { //using colors of original graph
+		if (getValue(z[i])>0.0) temp.set(i);
 	}
+	std::cout << " user cutting" << std::endl;
+	int num_c = get_components(g, temp, components);
+	if (num_c > 1) {
+		std::cout << "add user cut" << std::endl;
+		db temp1(size);
+		std::tie(it, end) = edges(g);
+		IloExpr expr(getEnv());
+		while (it != end) {
+			if (components[source(*it, g)] != components[target(*it, g)]) {
+				if (components[source(*it, g)] == 0 || components[target(*it, g)] == 0) {
+					temp1.set(colors[*it]);
+				}
+			}
+			++it;
+		}
+		for (int i = 0; i < z.getSize(); ++i) if (temp1.test(i))expr += z[i];
+		add(expr >= 1).end();
+		expr.end();
+	}
+	else std::cout << "is good" << std::endl;
+}
 
+
+
+ILOLAZYCONSTRAINTCALLBACK2(LazyCallback, IloBoolVarArray,z,graph_t,g) {
+	int size = z.getSize();
+	db temp(size);
+	std::vector<int> components(num_vertices(g));
+	auto colors = get_colors(g);
+	graph_traits<graph_t>::edge_iterator it, end;
+	for (int i = 0; i < size  ; ++i) { //using colors of original graph
+		if (getValue(z[i])>0.0) temp.set(i);
+	}
+	std::cout << "lazy cutting"<<std::endl;
+	int num_c = get_components(g,temp,components);
+	if (num_c > 1) {
+		std::cout << "add cut" << std::endl;
+		db temp1(size);
+		std::tie(it,end) = edges(g);
+		IloExpr expr(getEnv());
+		while (it != end) {
+			if (components[source(*it,g)]!=components[target(*it, g)]) {
+				if (components[source(*it, g)] == 0 || components[target(*it, g)] == 0) {
+					temp1.set(colors[*it]);
+				}
+			}
+			++it;
+		}
+		for (int i = 0; i < z.getSize(); ++i) if(temp1.test(i))expr += z[i];
+		add(expr >= 1).end();
+		expr.end();
+	}
+	else std::cout << "found optimal" << std::endl;
 }
 
 
@@ -40,8 +142,15 @@ void print_edges(EdgeIter first, EdgeIter last, const Graph& G) {
 		std::cout << "Edge: " << "(" << source(*it, G) << "," << target(*it, G) << ") " << " Color: " << colors[*it] << "\n";
 		std::cout << "Edge: " << "(" << target(*it, G) << "," << source(*it, G) << ") " << " Color: " << colors[*it] << "\n";
 	}
-	cout << " Number of vertex: " << num_vertices(G) << std::endl;
-	cout << " Number of edges: " << num_edges(G) << std::endl;
+	std::cout << " Number of vertex: " << num_vertices(G) << std::endl;
+	std::cout << " Number of edges: " << num_edges(G) << std::endl;
+	std::vector<int> components(num_vertices(G));
+	int num = connected_components(G, &components[0]);
+	std::vector<int>::size_type i;
+	std::cout << "Total number of components: " << num << std::endl;
+	for (i = 0; i != components.size(); ++i)
+		std::cout << "Vertex " << i << " is in component " << components[i] << std::endl;
+	std::cout << std::endl;
 }
 template<class Graph>
 void buildCCutModel(IloModel mod,IloBoolVarArray Z, const int k, const Graph &g) {
@@ -90,9 +199,8 @@ void buildCCutModel(IloModel mod,IloBoolVarArray Z, const int k, const Graph &g)
 int main()
 {
 	enum { A, B, C, D, E, F, G, H };
-	typedef adjacency_list<vecS, vecS, undirectedS, no_property, property<edge_color_t, int>> Graph;
 	typedef std::pair<int, int> Edge;
-	Graph::edge_iterator it, end;
+	graph_t::edge_iterator it, end;
 	const int n_vertices = 14;
 	const int k = 4;
 	Edge edge_array[] = {
@@ -105,7 +213,7 @@ int main()
 	int n_edges = sizeof(edge_array) / sizeof(edge_array[0]);
 	const int colors[] = { H,H,D,D,C,F,E,D,C,F,G,E,A,B,G,A,B };
 
-	Graph g(edge_array, edge_array + n_edges, colors, n_vertices);
+	graph_t g(edge_array, edge_array + n_edges, colors, n_vertices);
 	//add edges to super source vertex index 0. remember!!!
 	std::unordered_set<int> st(colors, colors + sizeof(colors) / sizeof(colors[0]));
 	int n_colors = st.size();
@@ -132,19 +240,19 @@ int main()
 		//cuts
 
 		cplex.use(MyCuts(env,Z,g));
-		//cplex.use(LazyCallback(env,Z));
+	    cplex.use(LazyCallback(env,Z,g));
 
 		//paramenters
-		//cplex.setParam(IloCplex::Param::MIP::Display,5);
-		//cplex.setParam(IloCplex::Param::Tune::Display,3);
-		//cplex.setParam(IloCplex::Param::Simplex::Display, 2);
-
+		cplex.setParam(IloCplex::Param::MIP::Display,5);
+		cplex.setParam(IloCplex::Param::Tune::Display,3);
+		cplex.setParam(IloCplex::Param::Simplex::Display, 2);
+		cplex.setParam(IloCplex::Param::Preprocessing::Presolve, 0);
 		cplex.solve();
 		cplex.out() << "solution status = " << cplex.getStatus() << endl;
 
 		cplex.out() << endl;
-		cplex.out() << "Number of components   = " << cplex.getObjValue() << endl;
-		for (int i = 0; i <= Z.getSize() - n_vertices; i++) {
+		cplex.out() << "Number of components of original problem  = " << cplex.getObjValue() << endl;
+		for (int i = 0; i < Z.getSize(); i++) {
 			cplex.out() << "  Z" << i << " = " << cplex.getValue(Z[i]) << endl;
 		}
 
