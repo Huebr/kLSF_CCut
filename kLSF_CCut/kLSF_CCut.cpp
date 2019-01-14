6db575ac7c392099a36a6d8bd90cf0d0f74ff63e
HueBr <pedro.jorge@aluno.uece.br>
2019-01-14 12:35:38 -0300

    option colocado

--- kLSF_CCut/kLSF_CCut.cpp
+++ kLSF_CCut/kLSF_CCut.cpp
@@ -3,14 +3,22 @@
 #include <algorithm>                 // for std::for_each
 #include <boost/graph/graph_traits.hpp> // for creation of descriptors vertex and edges.
 #include <boost/graph/adjacency_list.hpp> //for usage of adjacency list
-#include <boost/graph/filtered_graph.hpp>
+#include <boost/graph/graphml.hpp>
 #include <boost/graph/connected_components.hpp>
 #include <ilcplex/ilocplex.h>
-#include <unordered_set>
 #include <boost/dynamic_bitset.hpp>
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/graph/filtered_graph.hpp>
+#include <boost/exception/all.hpp>
+#include <exception>
+#include <vector>
 ILOSTLBEGIN //initialization make vs work properly
 
 using namespace boost;
+namespace po = boost::program_options;
 
 //basic definitions
 typedef typename adjacency_list<vecS, vecS, undirectedS, no_property, property<edge_color_t, int>> graph_t;
@@ -72,10 +80,10 @@ ILOUSERCUTCALLBACK2(MyCuts, IloBoolVarArray, z, graph_t, g) {
 	for (int i = 0; i < size; ++i) { //using colors of original graph
 		if (getValue(z[i])>0.0) temp.set(i);
 	}
-	std::cout << " user cutting" << std::endl;
+	//std::cout << " user cutting" << std::endl;
 	int num_c = get_components(g, temp, components);
 	if (num_c > 1) {
-		std::cout << "add user cut" << std::endl;
+		//std::cout << "add user cut" << std::endl;
 		db temp1(size);
 		std::tie(it, end) = edges(g);
 		IloExpr expr(getEnv());
@@ -91,7 +99,7 @@ ILOUSERCUTCALLBACK2(MyCuts, IloBoolVarArray, z, graph_t, g) {
 		add(expr >= 1).end();
 		expr.end();
 	}
-	else std::cout << "is good" << std::endl;
+	//else std::cout << "is good" << std::endl;
 }
 
 
@@ -105,10 +113,10 @@ ILOLAZYCONSTRAINTCALLBACK2(LazyCallback, IloBoolVarArray,z,graph_t,g) {
 	for (int i = 0; i < size  ; ++i) { //using colors of original graph
 		if (getValue(z[i])>0.0) temp.set(i);
 	}
-	std::cout << "lazy cutting"<<std::endl;
+	//std::cout << "lazy cutting"<<std::endl;
 	int num_c = get_components(g,temp,components);
 	if (num_c > 1) {
-		std::cout << "add cut" << std::endl;
+		//std::cout << "add cut" << std::endl;
 		db temp1(size);
 		std::tie(it,end) = edges(g);
 		IloExpr expr(getEnv());
@@ -124,7 +132,7 @@ ILOLAZYCONSTRAINTCALLBACK2(LazyCallback, IloBoolVarArray,z,graph_t,g) {
 		add(expr >= 1).end();
 		expr.end();
 	}
-	else std::cout << "found optimal" << std::endl;
+	//else std::cout << "found optimal" << std::endl;
 }
 
 
@@ -195,38 +203,8 @@ void buildCCutModel(IloModel mod,IloBoolVarArray Z, const int k, const Graph &g)
 
 
 
-
-int main()
-{
-	enum { A, B, C, D, E, F, G, H };
-	typedef std::pair<int, int> Edge;
-	graph_t::edge_iterator it, end;
-	const int n_vertices = 14;
-	const int k = 4;
-	Edge edge_array[] = {
-		Edge(1,2),  Edge(1,12), Edge(2,3),  Edge(3,4),
-		Edge(4,5),  Edge(5,6),  Edge(5,8),  Edge(5,9),
-		Edge(5,11), Edge(5,13), Edge(6,7),  Edge(7,8),
-		Edge(8,9),  Edge(9,10), Edge(10,11),Edge(11,12),
-		Edge(12,13),
-	};
-	int n_edges = sizeof(edge_array) / sizeof(edge_array[0]);
-	const int colors[] = { H,H,D,D,C,F,E,D,C,F,G,E,A,B,G,A,B };
-
-	graph_t g(edge_array, edge_array + n_edges, colors, n_vertices);
-	//add edges to super source vertex index 0. remember!!!
-	std::unordered_set<int> st(colors, colors + sizeof(colors) / sizeof(colors[0]));
-	int n_colors = st.size();
-	st.clear();
-	for (int i = 1; i < n_vertices; ++i) boost::add_edge(0, i, property<edge_color_t, int>(n_colors + i - 1), g);
-	std::tie(it, end) = boost::edges(g);
-	print_edges(it, end, g);
-
-
-	//temporario contar numero de cores
-	n_colors += n_vertices - 1;
-
-
+template<class Graph>
+void solveModel(int n_vertices, int n_colors, int k, Graph &g) {
 	//starting cplex code part
 	IloEnv   env; //environment
 	try {
@@ -235,36 +213,131 @@ int main()
 		buildCCutModel(model, Z, k, g);
 		IloCplex cplex(model);
 		cplex.exportModel("kSLF_CCut_relaxed.lp"); // good to see if the model is correct
-											//cross your fingers
+												   //cross your fingers
 
-		//cuts
+												   //cuts
 
-		cplex.use(MyCuts(env,Z,g));
-	    cplex.use(LazyCallback(env,Z,g));
+		cplex.use(MyCuts(env, Z, g));
+		cplex.use(LazyCallback(env, Z, g));
 
 		//paramenters
-		cplex.setParam(IloCplex::Param::MIP::Display,5);
-		cplex.setParam(IloCplex::Param::Tune::Display,3);
-		cplex.setParam(IloCplex::Param::Simplex::Display, 2);
+		//cplex.setParam(IloCplex::Param::MIP::Display, 5);
+		//cplex.setParam(IloCplex::Param::Tune::Display, 3);
+		//cplex.setParam(IloCplex::Param::Simplex::Display, 2);
 		cplex.setParam(IloCplex::Param::Preprocessing::Presolve, 0);
 		cplex.solve();
 		cplex.out() << "solution status = " << cplex.getStatus() << endl;
 
 		cplex.out() << endl;
 		cplex.out() << "Number of components of original problem  = " << cplex.getObjValue() << endl;
+		cplex.out() << "color(s) solution:";
 		for (int i = 0; i < Z.getSize(); i++) {
-			cplex.out() << "  Z" << i << " = " << cplex.getValue(Z[i]) << endl;
+			if(cplex.getValue(Z[i])==1) cplex.out() << "  " << i;
 		}
-
+		cplex.out() << std::endl;
 	}
 	catch (IloException& e) {
-		cerr << "Concert exception caught: " << e << endl;
+		std::cerr << "Concert exception caught: " << e << endl;
 	}
 	catch (...) {
-		cerr << "Unknown exception caught" << endl;
+		std::cerr << "Unknown exception caught" << endl;
 	}
 	//memory cleaning
 	env.end();
+	
+}
+
+
+int main(int argc, const char *argv[])
+{
+	typedef adjacency_list<vecS, vecS, undirectedS, no_property, property<edge_color_t, int>> Graph;
+	typedef std::pair<int, int> Edge;
+	typedef boost::graph_traits<Graph>::vertex_descriptor vertex_t;
+	Graph::edge_iterator it, end;
+	Graph g;
+	int n_vertices, n_colors;
+	//command-line processor
+
+	try {
+		std::ifstream ifn;
+		po::options_description desc{ "Options" };
+		desc.add_options()("help,h", "produce help message")
+			("input-file,i", po::value< string >(), "input file")
+			("include-path,I", po::value< string >(), "include path")
+			("setup-file", po::value< string >(), "setup file");
+		po::positional_options_description p;
+		p.add("input-file", -1);
+
+
+		po::variables_map vm;
+		po::store(po::command_line_parser(argc, argv).
+			options(desc).positional(p).run(), vm);
+		po::notify(vm);
+
+		if (vm.count("help")) {
+			cout << desc << "\n";
+			return 1;
+		}
+		else if (vm.count("input-file"))
+		{
+			std::cout << "Input files are: " << vm["input-file"].as<string>() << "\n";
+			if (vm.count("include-path")) {
+				ifn.open((vm["include-path"].as<string>() + vm["input-file"].as<string>()).c_str(), ifstream::in);
+				std::cout << "Include Path is " << vm["include-path"].as<string>() << "\n";
+			}
+			else ifn.open(vm["input-file"].as<string>().c_str(), ifstream::in);
+			if (!ifn.is_open()) {
+				std::cout << "error opening file" << std::endl;
+				exit(EXIT_FAILURE);
+			}
+			dynamic_properties dp;
+			dp.property("color", get(edge_color, g));
+			read_graphml(ifn, g, dp);
+
+			vector<string> vecI;
+			split(vecI, vm["input-file"].as<string>(), is_any_of("-."), token_compress_off);
+			if (vecI.size() == 6) {
+				std::cout << vecI[0] << std::endl;
+				n_vertices = stoi(vecI[0]);
+				std::cout << vecI[2] << std::endl;
+				n_colors = stoi(vecI[2]);
+				std::cout << vecI[3] << std::endl;
+				int k = stoi(vecI[3]);
+				//add edges to super source vertex. remember!!!
+				vertex_t u = add_vertex(g);
+				n_vertices++;
+				for (int i = 0; i < n_vertices - 1; ++i) boost::add_edge(u, i, property<edge_color_t, int>(n_colors++), g);
+				std::tie(it, end) = boost::edges(g);
+				//print_edges(it, end, g);
+				solveModel(n_vertices, n_colors, k, g);
+			}
+			else {
+				std::cout << "file wrong name format." << std::endl;
+			}
+
+		}
+		else if (vm.count("setup-file")) {
+			std::cout << "Not implemented yet" << std::endl;
+		}
+		else {
+			std::cout << "see options(-h)." << std::endl;
+		}
+
+
+	}
+	catch (const po::error &ex) {
+		std::cout << ex.what();
+		exit(EXIT_FAILURE);
+	}
+	catch (boost::exception &ex) {
+		std::cout << boost::diagnostic_information(ex) << std::endl;
+	}
+	catch (std::exception &ex) {
+		std::cout << ex.what();
+		exit(EXIT_FAILURE);
+	}
 
 	return 0;
 }
+
+
